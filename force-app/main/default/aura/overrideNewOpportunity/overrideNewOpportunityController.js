d6c30ad97817aad6df1e90c361d42058e683632d
Elena Garrido <cloudshadowforce@gmail.com>
2026-01-31 18:03:38 +0100

    Si es de B2B, redirige a la creación de una nueva oportunidad por RT, si no, muestra un mensaje

--- force-app/main/default/aura/overrideNewOpportunity/overrideNewOpportunityController.js
+++ force-app/main/default/aura/overrideNewOpportunity/overrideNewOpportunityController.js
@@ -9,12 +9,21 @@
     		action.setCallback(this, function(response) {
         		var state = response.getState();
         		if (state === "SUCCESS") {
-            		// do something with response.getReturnValue(), such as firing your create event here.
                     console.log("...:::: ¿ Es de B2B ? ::::...")
                     if(response.getReturnValue() === true){
                         console.log(':::::.... YES ....::::');
+                        // SI ES DE B2B, SE REDIRECCIONA
+                        var createRecordEvent = $A.get("e.force:createRecord");
+                        createRecordEvent.setParams({
+                            "entityApiName": "Opportunity",
+                            "recordTypeId" : recordTypeId
+                        });
+                        createRecordEvent.fire();
                     } else {
                     	console.log(':::::.... NO ....::::');
+                        // SI NO ES DE B2B, SE MUESTRA UN MENSAJE Y SE CIERRA EL MODAL
+                        alert('NO ES POSIBLE CREAR UNA OPORTUNIDAD DE FORMA MANUAL.');
+                        component.set("v.notB2B", true);
                     }
         		} else {
             		console.log("Failed with state: " + state);
